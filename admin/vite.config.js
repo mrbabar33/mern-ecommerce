f5c42465cfa2aa23587f924198bf693d9a0284e0
Babar Ali <mrbabarali3322@gmail.com>
2026-09-14 09:17:16 -0700

    updated admin panel

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
-    port: 5174
-  }
+    port: 5174,
+  },
 })
